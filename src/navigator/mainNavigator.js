b7ec1263c513aaff816f0b9bda61159c57e3ea4c
Crowdbotics <team@crowdbotics.com>
2020-10-30 07:44:03 +0000

    add package blueprint-react-native-blank-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen013565Navigator from '../features/BlankScreen013565/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen013565: { screen: BlankScreen013565Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
